cfd354ed2b4b1583e80249264a83c035c3e7b670
pavitpim40_mbp <pavitpim@gmail.com>
2024-05-03 10:23:28 +0700

    finish:fetch

--- 13-fetch.js
+++ 13-fetch.js
@@ -14,14 +14,69 @@ import fetch from 'node-fetch';
 
 async function getAllTodo() {
   try {
-    let response = await fetch('https://jsonplaceholder.typicode.com/users?website'); // new Promise(fulfill,reject) {}
+    let response = await fetch('https://jsonplaceholder.typicode.com/users'); // new Promise(fulfill,reject) {}
     let data = await response.json(); // Promise.resolve(response.json())
     console.log(data);
   } catch (error) {
     console.log(error);
   }
 }
-getAllTodo();
+// getAllTodo();
 
+// ถ้าต้องเรียก Request ด้วย method อื่นทำยังไง
 // GET,DELETE - USER
+async function getUserById(id) {
+  try {
+    let response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function deleteUserById(id) {
+  try {
+    let response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+      method: 'DELETE',
+    });
+    console.log(response.status);
+  } catch (error) {
+    console.log(error);
+  }
+}
+// deleteUserById(1);
+
 // PATCH,POST - USER
+async function createUser(user) {
+  try {
+    let response = await fetch(`https://jsonplaceholder.typicode.com/users`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(user), //JSON.stringify({ name: 'John', age: 27 })
+    });
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// createUser({ name: 'John', age: 27 });
+// createUser({ name: 'Jane', age: 32 });
+
+async function updateUser(userId, updateData) {
+  try {
+    let response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(updateData), //JSON.stringify({ name: 'John', age: 27 })
+    });
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+updateUser(1, { name: 'Jame', username: 'Jami' });
